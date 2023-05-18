27fd0f9f0a92378dae58cecb1e1203a51fb6d682
Christopher Baboolal <dlg.chris954@gmail.com>
2023-05-18 13:30:00 -0400

    Player Sprite Handler
    Horizontal and Idle Movement Only

---
+++ Assets/Scripts/PlayerSpriteHandler.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(SpriteRenderer))]
+public class PlayerSpriteHandler : MonoBehaviour
+{
+    private Rigidbody2D body;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+
+    private float horizontalSpeed = 5f;
+    private float horizontalInput;
+    private bool isFacingRight = true;
+
+    void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        if (horizontalInput < 0 && isFacingRight)
+        {
+            spriteRenderer.flipX = true;
+            isFacingRight = false;
+        }
+        else if (horizontalInput > 0 && !isFacingRight)
+        {
+            spriteRenderer.flipX = false;
+            isFacingRight = true;
+        }
+
+        animator.SetFloat("Speed", Mathf.Abs(body.velocity.x));
+    }
+
+    private void FixedUpdate()
+    {
+        body.velocity = new Vector2(horizontalInput * horizontalSpeed, body.velocity.y);
+    }
+}
+
