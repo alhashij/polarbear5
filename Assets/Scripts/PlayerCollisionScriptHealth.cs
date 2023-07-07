64d3857027d9e54a3337b4184868e316b704f931
Christopher Baboolal <dlg.chris954@gmail.com>
2023-07-07 00:48:22 -0400

    Created Checkpoint and Hazard Reset
    The hazards will now make you lose a life and reset to your respawn or checkpoint position.

--- Assets/Scripts/PlayerCollisionScriptHealth.cs
+++ Assets/Scripts/PlayerCollisionScriptHealth.cs
@@ -4,29 +4,84 @@ using UnityEngine;
 
 public class PlayerCollisionScriptHealth : MonoBehaviour
 {
+    private bool isColliding = false;
+    private bool canTakeDamage = true; // Add flag to control damage
+    private Vector3 spawnPosition; // Store the spawn position
+    private Vector3 checkpointPosition; // Store the checkpoint position
+    private Rigidbody2D rb; // Reference to the player's Rigidbody2D component
+
+    private void Start()
+    {
+        spawnPosition = transform.position; // Store the initial spawn position
+        checkpointPosition = spawnPosition; // Set the initial checkpoint position to the spawn position
+        rb = GetComponent<Rigidbody2D>(); // Get the Rigidbody2D component
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.transform.tag == "Hazard")
+        if (collision.transform.CompareTag("Hazard") && !isColliding && canTakeDamage)
         {
-
-            /* //displays the game over screen when player comes into contact with the hazard
-             PlayerManager.GameOverScreen = true;
-             //disables player from moving once at game over screen,
-             //it disables the full screen and puts the canvas game over screen only
-                 gameObject.SetActive(false);
-             Debug.Log("Test");
-            */
-
+            isColliding = true;
+            canTakeDamage = false; // Disable damage temporarily
 
             PlayerHealth.maxplayerhealth--;
+
             if (PlayerHealth.maxplayerhealth <= 0)
             {
                 PlayerManager.GameOverScreen = true;
                 gameObject.SetActive(false);
 
-                Debug.Log("players health at 0 now game over");
+                Debug.Log("Player's health is now 0. Game over.");
+            }
+            else
+            {
+                StartCoroutine(EnableDamage()); // Enable damage after a delay
+                StartCoroutine(DisableMovementForSeconds(2f)); // Disable player movement for 2 seconds
+                ResetToCheckpointPosition(); // Reset the player to the checkpoint position
+            }
+        }
+    }
 
+    private IEnumerator EnableDamage()
+    {
+        yield return new WaitForSeconds(1f); // Adjust the delay as needed
+        canTakeDamage = true; // Enable damage
+    }
 
+    private IEnumerator DisableMovementForSeconds(float seconds)
+    {
+        rb.constraints = RigidbodyConstraints2D.FreezeAll; // Freeze player movement
+        yield return new WaitForSeconds(seconds);
+        rb.constraints = RigidbodyConstraints2D.None; // Unfreeze player movement
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Hazard"))
+        {
+            isColliding = false;
+        }
+    }
+
+    private void ResetToCheckpointPosition()
+    {
+        rb.velocity = Vector2.zero; // Stop the player's current velocity
+        transform.position = checkpointPosition; // Reset the player position to the checkpoint position
+    }
+
+    public void SetCheckpoint(Vector3 position)
+    {
+        checkpointPosition = position; // Set the checkpoint position to the provided position
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Checkpoint"))
+        {
+            CheckpointScript checkpoint = collision.GetComponent<CheckpointScript>();
+            if (checkpoint != null)
+            {
+                checkpoint.SetPlayerCheckpoint(this); // Set the player's checkpoint position
             }
         }
     }
